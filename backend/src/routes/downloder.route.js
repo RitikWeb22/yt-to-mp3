c7e673af1a9c840f9b4c44694dbfeac2fa98972d
Ritik <114500316+RitikWeb22@users.noreply.github.com>
2026-03-18 22:29:02 +0530

    Add download limiter to download route

--- backend/src/routes/downloder.route.js
+++ backend/src/routes/downloder.route.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 
 const downloadRouter = Router();
-const { infoController, downloadController, historyController } = require('../controllers/downloader.controller');
+const { infoController, downloadController, historyController, downloadLimiter } = require('../controllers/downloader.controller');
 
 // info route
 downloadRouter.get('/info', infoController);
 
 // download route
-downloadRouter.get('/download', downloadController);
+downloadRouter.get('/download', downloadLimiter, downloadController);
 
 // history route
 downloadRouter.get('/history', historyController);

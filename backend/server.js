b9fb385c9cc409d73afed42458703644f99016b5
Ritik <114500316+RitikWeb22@users.noreply.github.com>
2026-03-18 19:31:23 +0530

    Change PORT to a URL instead of a number

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./src/config/db');
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || "https://yt-to-mp3-dmm4.onrender.com";
 // Database connect
 connectDB();
 
